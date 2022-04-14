c7b5abb7cccdde4776b0372cffee5b87ad01edaf
liangbo <15267214967@163.com>
2022-04-14 16:13:57 +0800

    first commit

---
+++ src/test/java/Aa.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Aa {
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<String>(1);
+        list.add("1");
+        list.add("2");
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+    }
+}
