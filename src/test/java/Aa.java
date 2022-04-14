2d0bf20a7e83f8e404e0743b511df728960ddde9
liangbo <15267214967@163.com>
2022-04-14 16:49:57 +0800

    修改代码

--- src/test/java/Aa.java
+++ src/test/java/Aa.java
@@ -7,7 +7,7 @@ public class Aa {
     public static void main(String[] args) {
         List<String> list=new ArrayList<String>(1);
         list.add("1");
-        list.add("222222");
+        list.add("33333");
         for (String s : list) {
             System.out.println(s);
         }
